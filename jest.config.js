91045a933ad27e7e88aea987118f4574e98bcc44
vadim_budarin <budarin-vv@yandex.ru>
2023-10-31 07:20:45 +0300

    upgrade deps

--- jest.config.js
+++ jest.config.js
@@ -85,7 +85,10 @@ module.exports = {
     moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node'],
 
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-    // moduleNameMapper: {},
+    moduleNameMapper: {
+        '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)':
+            path.join(__dirname, './jest/assetsTransformer.js'),
+    },
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
     // modulePathIgnorePatterns: [],
