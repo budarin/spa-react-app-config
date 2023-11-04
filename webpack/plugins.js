d5d63f153f20979dfeb61f35a6ec62ac73170ff8
Vadim Budarin <budarin-vv@yandex.ru>
2023-11-04 14:34:17 +0300

    add webpack-assets-manifest

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -1,7 +1,9 @@
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackAssetsManifest = require('webpack-assets-manifest');
 
 module.exports = {
     HtmlWebpackPlugin,
     CopyPlugin,
+    WebpackAssetsManifest,
 };
