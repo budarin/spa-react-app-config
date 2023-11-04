d1cf3a6ac8bc52c1a488f22b347d8379653ecc41
Vadim Budarin <budarin-vv@yandex.ru>
2023-11-04 14:07:32 +0300

    fix no-restricted-syntax eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -106,6 +106,20 @@ module.exports = {
                 varsIgnorePattern: '^_',
             },
         ],
+
+        'no-restricted-syntax': [
+            'error',
+            {
+                selector: 'LabeledStatement',
+                message:
+                    'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+            },
+            {
+                selector: 'WithStatement',
+                message:
+                    '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+            },
+        ],
     },
 
     ignorePatterns: ['*.js', '!./src'],
