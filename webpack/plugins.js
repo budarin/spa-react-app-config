4c737df4b81c1b01166c2f0e13eb60ae74d00766
Vadim Budarin <budarin-vv@yandex.ru>
2023-11-19 17:46:14 +0300

    fix bug

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -2,7 +2,6 @@ const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
 const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizePlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     HtmlWebpackPlugin,
