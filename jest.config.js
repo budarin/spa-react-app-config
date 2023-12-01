e1cba0167daa6595def9ea9c22273d7d20247250
Vadim Budarin <budarin-vv@yandex.ru>
2023-12-01 22:39:54 +0300

    add LOG_LEVEL var

--- jest.config.js
+++ jest.config.js
@@ -72,6 +72,7 @@ module.exports = {
         __DEV__: process.env['NODE_ENV'] !== 'production',
         __PROD__: process.env['NODE_ENV'] === 'production',
         __TEST__: process.env['NODE_ENV'] === 'test',
+        __LOG_LEVEL__: process.env['LOG_LEVEL'] || 'info',
         IS_REACT_ACT_ENVIRONMENT: true,
     },
 
--- webpack/client.config.js
+++ webpack/client.config.js
@@ -15,6 +15,7 @@ module.exports = {
             __DEV__: process.env['NODE_ENV'] !== 'production',
             __PROD__: process.env['NODE_ENV'] === 'production',
             __TEST__: process.env['NODE_ENV'] === 'test',
+            __LOG_LEVEL__: process.env['LOG_LEVEL'] || 'info',
         }),
     ],
     resolve: {
