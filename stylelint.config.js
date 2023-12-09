1925c02dafc91cb8d133a168e35e4830428f961a
vadim_budarin <budarin-vv@yandex.ru>
2023-12-10 01:07:11 +0300

    add stylelint-use-logical

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: ['stylelint-config-standard', 'stylelint-config-clean-order'],
-    plugins: ['stylelint-use-logical-spec'],
+    plugins: ['stylelint-use-logical'],
     rules: {
         'selector-class-pattern': null,
         'custom-property-pattern': null,
