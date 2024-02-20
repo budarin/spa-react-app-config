03b859c96a1e302c7b1c1dae7e50e8f32ebe92f6
vadim_budarin <budarin-vv@yandex.ru>
2024-02-20 04:31:20 +0300

    add stylint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,5 +5,19 @@ module.exports = {
         'selector-class-pattern': null,
         'custom-property-pattern': null,
         'liberty/use-logical-spec': 'always',
+
+        // https://defensivecss.dev/
+        // https://github.com/yuschick/stylelint-plugin-defensive-css
+        'plugin/use-defensive-css': [true, { 'accidental-hover': true }],
+        'plugin/use-defensive-css': [true, { 'background-repeat': true }],
+        'plugin/use-defensive-css': [true, { 'custom-property-fallbacks': true }],
+        'plugin/use-defensive-css': [
+            true,
+            { 'custom-property-fallbacks': [true, { ignore: [/hel-/, 'theme-'] }] },
+        ],
+        'plugin/use-defensive-css': [true, { 'flex-wrapping': true }],
+        'plugin/use-defensive-css': [true, { 'scroll-chaining': true }],
+        'plugin/use-defensive-css': [true, { 'scrollbar-gutter': true }],
+        'plugin/use-defensive-css': [true, { 'vendor-prefix-grouping': true }],
     },
 };
