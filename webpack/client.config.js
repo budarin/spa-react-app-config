7d6ef7904a064899177c242f7212814944943d63
vadim_budarin <budarin-vv@yandex.ru>
2024-05-17 01:11:13 +0300

    fix

--- webpack/client.config.js
+++ webpack/client.config.js
@@ -2,121 +2,117 @@ const path = require('path');
 const webpack = require('webpack');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-async function getConfig() {
-    const browserslistToEsbuild = await import('browserslist-to-esbuild');
+const browserslistToEsbuild = require('./browserslist-to-esbuild.js');
 
-    const config = {
-        mode: 'development',
-        devtool: 'inline-source-map',
-        entry: './src/client/index.ts',
-        output: {
-            path: path.resolve('./dist'),
-            trustedTypes: {
-                policyName: 'webpack-tt',
-            },
+const config = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/client/index.ts',
+    output: {
+        path: path.resolve('./dist'),
+        trustedTypes: {
+            policyName: 'webpack-tt',
         },
-        plugins: [
-            new webpack.DefinePlugin({
-                __DEBUG__: process.env['DEBUG'] === 'true',
-                __DEV__: process.env['NODE_ENV'] !== 'production',
-                __PROD__: process.env['NODE_ENV'] === 'production',
-                __TEST__: process.env['NODE_ENV'] === 'test',
-                __VERSION__: JSON.stringify(process.env.npm_package_version),
-            }),
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            __DEBUG__: process.env['DEBUG'] === 'true',
+            __DEV__: process.env['NODE_ENV'] !== 'production',
+            __PROD__: process.env['NODE_ENV'] === 'production',
+            __TEST__: process.env['NODE_ENV'] === 'test',
+            __VERSION__: JSON.stringify(process.env.npm_package_version),
+        }),
+    ],
+    resolve: {
+        extensions: [
+            '.ts',
+            '.tsx',
+            '.js',
+            '.jsx',
+            '.jsm',
+            '.json',
+            '.css',
+            '.png',
+            '.svg',
+            '.jpg',
+            '.jpeg',
+            '.gif',
+            '.ogg',
+            '.mp3',
+            '.wav',
+            '.ico',
+            '.xml',
+            '.woff2',
         ],
-        resolve: {
-            extensions: [
-                '.ts',
-                '.tsx',
-                '.js',
-                '.jsx',
-                '.jsm',
-                '.json',
-                '.css',
-                '.png',
-                '.svg',
-                '.jpg',
-                '.jpeg',
-                '.gif',
-                '.ogg',
-                '.mp3',
-                '.wav',
-                '.ico',
-                '.xml',
-                '.woff2',
-            ],
-            modules: ['node_modules', 'src'],
-            plugins: [
-                // вычисляем путь к конфигу при инициализации webpack.config.js
-                new TsconfigPathsPlugin(),
-            ],
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav|ico|xml|woff2)$/i,
-                    type: 'asset/resource',
-                },
-                {
-                    test: /\.(ts|tsx|js|jsx|json)$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: 'esbuild-loader',
-                            options: {
-                                // browserslistToEsbuild('>0.2%, not dead')
-                                target: browserslistToEsbuild(),
-                            },
+        modules: ['node_modules', 'src'],
+        plugins: [
+            // вычисляем путь к конфигу при инициализации webpack.config.js
+            new TsconfigPathsPlugin(),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav|ico|xml|woff2)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(ts|tsx|js|jsx|json)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'esbuild-loader',
+                        options: {
+                            // browserslistToEsbuild('>0.2%, not dead')
+                            target: browserslistToEsbuild(),
                         },
-                    ],
-                },
-                {
-                    test: /\.css$/i,
-                    use: [
-                        'style-loader',
-                        'css-loader',
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                sourceMap: true,
-                                postcssOptions: {
-                                    plugins: [
-                                        'postcss-preset-env',
-                                        [
-                                            'postcss-pixels-to-rem',
-                                            {
-                                                base: 16,
-                                                unit: 'rem',
-                                                exclude: [],
-                                                mediaQueries: true,
-                                            },
-                                        ],
+                    },
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            postcssOptions: {
+                                plugins: [
+                                    'postcss-preset-env',
+                                    [
+                                        'postcss-pixels-to-rem',
+                                        {
+                                            base: 16,
+                                            unit: 'rem',
+                                            exclude: [],
+                                            mediaQueries: true,
+                                        },
                                     ],
-                                },
+                                ],
                             },
                         },
-                    ],
-                },
-            ],
-        },
-
-        devServer: {
-            port: 3000,
-            client: {
-                overlay: false,
+                    },
+                ],
             },
-            static: {
-                publicPath: '/dist',
-                directory: './dist',
-            },
-            hot: true,
-            open: false,
-            compress: true,
-            historyApiFallback: true,
+        ],
+    },
+
+    devServer: {
+        port: 3000,
+        client: {
+            overlay: false,
         },
-    };
+        static: {
+            publicPath: '/dist',
+            directory: './dist',
+        },
+        hot: true,
+        open: false,
+        compress: true,
+        historyApiFallback: true,
+    },
+};
 
-    return config;
-}
-
-module.exports = getConfig();
+module.exports = config;
